1213cc1452ba5ac3456b13b25ac8165581302014
Lomooon <Alfarsi.loura@gmail.com>
2024-09-11 14:24:07 +0300

    code cleaned

--- main.js
+++ main.js
@@ -1,28 +1,23 @@
 let square = document.getElementById("square");
 
-
-
 function randomPosition(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 function randomColor() {
-    let letters = '0123456789ABCDEF';
-    let color = '#'
-    for (let i = 0; i < 6; i++){
-        color+= letters[Math.floor(Math.random() * 16)]
-    }
-    return color;
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 console.log();
 
 setInterval(() => {
-    square.style.top = randomPosition(0 ,50) + "vw";
-    square.style.left = randomPosition(0 ,100) + "vw";
-    square.style.width = randomPosition(20 , 400) + "px"
-    square.style.height = randomPosition(20, 400) + "px"
-    square.style.backgroundColor = randomColor();
-    
-    console.log("This will be printed");
-
-}, 2000)
+  square.style.top = randomPosition(0, 50) + "vw";
+  square.style.left = randomPosition(0, 100) + "vw";
+  square.style.width = randomPosition(20, 400) + "px";
+  square.style.height = randomPosition(20, 400) + "px";
+  square.style.backgroundColor = randomColor();
+}, 2000);
